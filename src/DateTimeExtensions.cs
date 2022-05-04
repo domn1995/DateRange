da09438410ea703267ce5c5e82833b02a48f29b2
Domn Werner <domn.werner@gmail.com>
2022-05-03 21:35:11 -0700

    Start DateTimeExtensions.

---
+++ src/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace DateRange;
+
+public static class DateTimeExtensions
+{
+    public static DateTime YearsAgo(this DateTime date, int years) => date.AddYears(-1 * years);
+
+    public static DateTime MonthsAgo(this DateTime date, int months) => date.AddMonths(-1 * months);
+
+    public static DateTime WeeksAgo(this DateTime date, double weeks) => date.AddDays(-7 * weeks);
+
+    public static DateTime DaysAgo(this DateTime date, double days) => date.AddDays(-1 * days);
+
+    public static DateTime MinutesAgo(this DateTime date, double minutes) => date.AddMinutes(-1 * minutes);
+
+    public static DateTime SecondsAgo(this DateTime date, double seconds) => date.AddSeconds(-1 * seconds);
+
+    public static DateTime MillisecondsAgo(this DateTime date, double milliseconds) => date.AddMilliseconds(-1 * milliseconds);
+}
