00b06971c1e7ceaa4914b62de4de740d5375c19d
Domn Werner <domn.werner@gmail.com>
2022-05-03 21:34:36 -0700

    Create DateOnlyRange.

---
+++ src/DateOnlyRange.cs
@@ -0,0 +1,27 @@
+﻿namespace DateRange;
+
+/// <summary>
+/// Models an inclusive date range without a meaningful time range.
+/// </summary>
+/// <param name="Start">The inclusive start date.</param>
+/// <param name="End">The inclusive end date.</param>
+public readonly record struct DateOnlyRange(DateOnly Start, DateOnly End)
+{
+    public bool Contains(DateOnly value) => Start <= value && value <= End;
+
+    public DateTimeRange ToDateTimeRange() => new(Start.ToDateTime(TimeOnly.MinValue), End.ToDateTime(TimeOnly.MinValue));
+
+    public TimeSpan ToTimeSpan() => ToDateTimeRange().ToTimeSpan();
+
+    public double TotalWeeks => ToDateTimeRange().TotalWeeks;
+
+    public double TotalDays => ToDateTimeRange().TotalDays;
+
+    public double TotalHours => ToDateTimeRange().TotalHours;
+
+    public double TotalMinutes => ToDateTimeRange().TotalMinutes;
+    
+    public double TotalSeconds => ToDateTimeRange().TotalSeconds;
+
+    public double TotalMilliseconds => ToDateTimeRange().TotalMilliseconds;
+}
